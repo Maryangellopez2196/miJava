57db986db2689375df0097b8e96aa6375efdc64e
Sofia Lopez <maryangel.lopez147@std.uni.edu.ni>
2025-06-23 00:05:07 -0600

    primer commit con todos los archivos del proyecto

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      
+String nombre = "Estudiante";
+
+int a = 5;
+int b = 3;
+int suma = a + b;
+
+System.out.println("Hola," + nombre + "!");
+System.out.println("la suma de:" + a + "+" + b + "es:" + suma);
+
+    }
+}
